46b34e1a42bf2c28ddbba7d182085a6450c73e14
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2019-08-28 16:45:55 -0400

    Updated tests

--- snow_day_phone_chain_test.go
+++ snow_day_phone_chain_test.go
@@ -6,6 +6,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestZero(t *testing.T) {
+	require := require.New(t)
+
+	minutes := uint32(0)
+	callsPerPerson := uint32(2)
+	result := CalculateNumPhoneCalls(minutes, callsPerPerson)
+	require.Equal(uint64(0), result)
+}
 func TestKnownValue2(t *testing.T) {
 	require := require.New(t)
 
@@ -13,13 +21,23 @@ func TestKnownValue2(t *testing.T) {
 	callsPerPerson := uint32(2)
 	result := CalculateNumPhoneCalls(minutes, callsPerPerson)
 	require.Equal(uint64(6), result)
+
+	minutes = 3
+	callsPerPerson = 2
+	result = CalculateNumPhoneCalls(minutes, callsPerPerson)
+	require.Equal(uint64(14), result)
 }
 
 func TestKnownValue3(t *testing.T) {
 	require := require.New(t)
 
-	minutes := uint32(3)
+	minutes := uint32(2)
 	callsPerPerson := uint32(3)
 	result := CalculateNumPhoneCalls(minutes, callsPerPerson)
+	require.Equal(uint64(12), result)
+
+	minutes = 3
+	callsPerPerson = 3
+	result = CalculateNumPhoneCalls(minutes, callsPerPerson)
 	require.Equal(uint64(39), result)
 }

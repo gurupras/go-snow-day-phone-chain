9feacf2595576963854bd7764da38149ee3ee14d
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2019-08-29 10:03:38 -0400

    Fixed bug where we were making 2 calls a minute rather than 1

--- snow_day_phone_chain.go
+++ snow_day_phone_chain.go
@@ -1,5 +1,45 @@
 package snowdayphonechain
 
+import "sync"
+
+type person struct {
+	callsRemaining uint32
+}
+
+func newPerson(callsPerPerson uint32) *person {
+	return &person{callsPerPerson}
+}
+
+type tickEvent int
+
+const tickActionCall tickEvent = 1
+const tickActionDone tickEvent = 2
+
+type tickAction struct {
+	action tickEvent
+	person *person
+}
+
+// For each person in the list, reduce the number of calls available by 1
+// If the person is done making callsPerPerson calls, then remove them from the persons list
+// We do this by using different events
+func (p *person) tick(eventChan chan *tickAction) {
+	var action *tickAction
+	if p.callsRemaining > 0 {
+		action = &tickAction{
+			tickActionCall,
+			p,
+		}
+		p.callsRemaining--
+	} else {
+		action = &tickAction{
+			tickActionDone,
+			p,
+		}
+	}
+	eventChan <- action
+}
+
 // CalculateNumPhoneCalls calculates the number of phone calls made within
 // numMinutes time at callsPerPerson number of calls per person
 func CalculateNumPhoneCalls(numMinutes uint32, callsPerPerson uint32) uint64 {
@@ -8,12 +48,60 @@ func CalculateNumPhoneCalls(numMinutes uint32, callsPerPerson uint32) uint64 {
 	// that we're not blowing up the stack
 	// We start with one person
 
-	var numPersons uint64 = 1
+	persons := make([]*person, 0)
+	p := newPerson(callsPerPerson)
+	persons = append(persons, p)
+
+	removePerson := func(p *person) {
+		targetIdx := -1
+		for idx, _p := range persons {
+			if _p == p {
+				targetIdx = idx
+				break
+			}
+		}
+		if targetIdx != -1 {
+			persons = append(persons[:targetIdx], persons[targetIdx+1:]...)
+		}
+	}
+
 	var numCalls uint64
 	for i := uint32(0); i < numMinutes; i++ {
-		callsMade := (uint64(numPersons) * uint64(callsPerPerson)) // The number of calls made this iteration
-		// In the next iteration, each of these people are going to make callsPerPerson calls
-		numPersons = callsMade
+		// The number of calls made this iteration
+		// Each person can only make *one* call a minute
+		tickChan := make(chan *tickAction)
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+
+		// Calls made this iteration
+		var callsMade uint64
+
+		go func() {
+			defer wg.Done()
+			for event := range tickChan {
+				switch event.action {
+				case tickActionCall:
+					callsMade++
+				case tickActionDone:
+					removePerson(event.person)
+				}
+			}
+		}()
+
+		// Iterate over all the available persons and either place a call or remove them from the list if they're done
+		tmp := make([]*person, len(persons))
+		copy(tmp, persons)
+		for _, p := range tmp {
+			p.tick(tickChan)
+		}
+		close(tickChan)
+		wg.Wait()
+
+		// For each call that was made, a new person joins the chain
+		for idx := uint64(0); idx < callsMade; idx++ {
+			p := newPerson(callsPerPerson)
+			persons = append(persons, p)
+		}
 		// Sum up the total number of calls made overall
 		numCalls += callsMade
 	}
--- snow_day_phone_chain_test.go
+++ snow_day_phone_chain_test.go
@@ -20,24 +20,24 @@ func TestKnownValue2(t *testing.T) {
 	minutes := uint32(2)
 	callsPerPerson := uint32(2)
 	result := CalculateNumPhoneCalls(minutes, callsPerPerson)
-	require.Equal(uint64(6), result)
+	require.Equal(uint64(3), result)
 
 	minutes = 3
 	callsPerPerson = 2
 	result = CalculateNumPhoneCalls(minutes, callsPerPerson)
-	require.Equal(uint64(14), result)
+	require.Equal(uint64(6), result)
 }
 
 func TestKnownValue3(t *testing.T) {
 	require := require.New(t)
 
-	minutes := uint32(2)
+	minutes := uint32(3)
 	callsPerPerson := uint32(3)
 	result := CalculateNumPhoneCalls(minutes, callsPerPerson)
-	require.Equal(uint64(12), result)
+	require.Equal(uint64(7), result)
 
-	minutes = 3
+	minutes = 4
 	callsPerPerson = 3
 	result = CalculateNumPhoneCalls(minutes, callsPerPerson)
-	require.Equal(uint64(39), result)
+	require.Equal(uint64(14), result)
 }
